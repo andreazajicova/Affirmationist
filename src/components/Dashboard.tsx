37204e127b16a4c8d804e7030e6e3a325716606f
Andrea Zajicova <35815182+andreazajicova@users.noreply.github.com>
2020-10-14 11:55:45 +0200

    Starting affirmations app

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AffirmationWall from './AffirmationWall';
+
+export default function Dashboard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
